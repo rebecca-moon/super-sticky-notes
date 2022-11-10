6e5fc84293a98934ff897ae5bb49581eaa742345
rebecca-moon <rebecca.n.moon@gmail.com>
2022-11-10 09:28:03 -0700

    Initialize version control

---
+++ src/Note.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Note = (props) => {
+  //edit title event handler
+  const updateTitle = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props.note.id;
+    props.onType(editMeId, "title", updatedValue);
+  };
+  //edit description event handler
+  const updateDescription = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props.note.id;
+    props.onType(editMeId, "description", updatedValue);
+  };
+  //remove note event handler
+  const clickDelete = () => {
+    props.removeNote(props.note.id);
+  };
+
+  return (
+    <li className="note">
+      <input
+        className="note__title"
+        type="text"
+        placeholder="Title"
+        value={props.note.title}
+        onChange={updateTitle}
+      />
+      <textarea
+        className="note__description"
+        placeholder="Description..."
+        value={props.note.description}
+        onChange={updateDescription}
+      />
+      <span className="note__delete" onClick={clickDelete}>
+        X
+      </span>
+    </li>
+  );
+};
+
+export default Note;
